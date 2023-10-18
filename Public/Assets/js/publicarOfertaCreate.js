a707e56becaff209a76ae103e0ecec3a7bc9278b
RobLucho <lucho.aguilar.hl.38@gmail.com>
2023-10-18 16:36:28 -0300

    código de publicar oferta front

---
+++ Public/Assets/js/publicarOfertaCreate.js
@@ -0,0 +1,20 @@
+const createOfertaForm = document.getElementById('ofertaAlquilerCreate');
+createOfertaForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    usuarioUpdate();
+});
+function usuarioUpdate(){
+    const formulario = document.getElementById('ofertaAlquilerCreate');
+    const formData = new FormData(formulario);
+    
+    fetch(URL_PATH + '/Usuario/update/', {method: 'POST', body: formData}
+    ).then(response => response.json()
+    ).then(data => {
+        if(data.success){
+            console.log(data.message);
+            window.location.replace(URL_PATH + '/OfertaAlquiler/home');
+        }else{
+            console.log(data.message);
+        }
+    });
+}
