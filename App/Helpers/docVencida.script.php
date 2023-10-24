2a91fa9eb55e16a097784490a53bb65329e1b2f8
RobLucho <lucho.aguilar.hl.38@gmail.com>
2023-10-24 15:24:33 -0300

    creacion de script para borrar documentacion vencida

--- App/Helpers/docVencida.script.php
+++ App/Helpers/docVencida.script.php
@@ -1,3 +1,15 @@
 <?php 
-    
+    require_once(__DIR__.'/../Services/conexion.php');
+    require_once(__DIR__.'/../Services/session.php');
+    require_once(__DIR__.'/../Model/controladorDeSessiones.php');
+    require_once(__DIR__.'/../Model/usuario.php');
+    require_once(__DIR__.'/../Model/documentacion.php');
+
+    $conexion = new Conexion();
+    $connect = $conexion->getConexion();
+    $session = new Session();
+
+    $userSession = new ControladorDeSessiones($session,$connect);
+    $usuario = new Usuario($connect);
+    $documentacion = new Documentacion($connect);
 ?>
